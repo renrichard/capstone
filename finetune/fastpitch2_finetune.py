f507567a898e4b14c838204d3d34c621fa12a566
martynwei <martyn.wei@gmail.com>
2021-09-13 18:39:57 -0400

    Working finetune without checkpoints

--- finetune/fastpitch2_finetune.py
+++ finetune/fastpitch2_finetune.py
@@ -30,7 +30,7 @@ def main(cfg):
     trainer = pl.Trainer(**cfg.trainer)
     exp_manager(trainer, cfg.get("exp_manager", None))
     model = FastPitchModel(cfg=cfg.model, trainer=trainer)
-    model.maybe_init_from_pretrained_checkpoint(cfg=cfg)
+    # model.maybe_init_from_pretrained_checkpoint(cfg=cfg)
     lr_logger = pl.callbacks.LearningRateMonitor()
     epoch_time_logger = LogEpochTimeCallback()
     trainer.callbacks.extend([lr_logger, epoch_time_logger])
