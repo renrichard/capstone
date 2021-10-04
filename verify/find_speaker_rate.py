0ae05847319381ddde80da755ab92b8cbcd26127
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2021-10-03 22:23:27 -0400

    Calculated mean and stdev of speaking rates

--- verify/find_speaker_rate.py
+++ verify/find_speaker_rate.py
@@ -1,7 +1,5 @@
 from glob import glob
-from json import dumps
 from os.path import join
-from re import sub
 
 from librosa.core import load
 
@@ -9,6 +7,7 @@ from preprocess.json.create_vcc_2020_json import get_num_to_transcription, get_d
 from preprocess.json.vcc_2020_constants import vcc_2020_target_sr, speakers
 from preprocess.json.vcc_2020_paths import synth_dir
 
+import numpy as np
 
 def find_speaker_rate(speaker):
 	num_to_transcription = get_num_to_transcription()
@@ -20,31 +19,31 @@ def find_speaker_rate(speaker):
 
 	synth_speaker_filenums_to_transcriptions = dict(zip(synth_speaker_filepaths, synth_speaker_transcriptions))
 
-	metadata = []
+	word_rates = []
+	char_rates = []
 
 	for fpath, t in synth_speaker_filenums_to_transcriptions.items():
-
-		file_metadata = dict()
 
 		num_words = len(t.split())
 		num_chars = len(t) - t.count(' ') - t.count('.')
 		target_data, _ = load(fpath, vcc_2020_target_sr)
 		duration = get_duration(target_data, vcc_2020_target_sr)
 
-		file_metadata['fpath'] = fpath
-		file_metadata['num_words'] = num_words
-		file_metadata['num_chars'] = num_chars
-		file_metadata['duration'] = duration
-		file_metadata['word_rate'] = num_words / duration
-		file_metadata['char_rate'] = num_chars / duration
+		word_rates.append(num_words / duration)
+		char_rates.append(num_chars / duration)
 
-		metadata.append(file_metadata)
+	word_rates = np.array(word_rates)
+	char_rates = np.array(char_rates)
 
-	fp = join(synth_speaker_path, '{}_duration.json'.format(speaker))
-	with open(fp, 'w') as f:
-		contents = dumps(metadata).strip('[]')
-		contents = sub('}, {', '}\n{', contents)
-		f.write(contents)
+	print('id:', speaker)
+	print('word/s mean: %.2f' % word_rates.mean())
+	print('word/s stdev: %.2f' % word_rates.std())
+	print('char/s mean: %.2f' % char_rates.mean())
+	print('char/s stdev: %.2f' % np.std(char_rates))
+	print()
+
+
 
 if __name__ == "__main__":
-	find_speaker_rate(speakers[0])
+	for speaker in speakers:
+		find_speaker_rate(speaker)
